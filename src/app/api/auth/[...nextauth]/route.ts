bf98322bc77fa11b9ece2b7c66c732b74b338a86
joaomatheusars <joaomatheusars@gmail.com>
2023-08-14 15:45:02 -0300

    t

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -19,5 +19,5 @@ const authOptions: AuthOptions = {
     ],
 };
 
-const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST};
+//const handler = NextAuth(authOptions);
+//export {handler as GET, handler as POST};
