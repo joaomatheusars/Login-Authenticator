0f22ab81cd8674c32444a8c9ded473b6927dbb96
joaomatheusars <joaomatheusars@gmail.com>
2023-08-14 15:37:51 -0300

    t

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import { PrismaAdapter} from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import { prisma } from "@/lib/prisma";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
     providers:[
         GoogleProvider({
@@ -19,5 +19,5 @@ export const authOptions: AuthOptions = {
     ],
 };
 
-//const handler = NextAuth(authOptions);
-//export {handler as GET, handler as POST};
+const handler = NextAuth(authOptions);
+export {handler as GET, handler as POST};
