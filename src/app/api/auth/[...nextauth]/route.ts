469e3d04f988dad8d2b56220f34b7396b6c67145
joaomatheusars <joaomatheusars@gmail.com>
2023-08-16 10:08:18 -0300

    Commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,11 +12,21 @@ const authOptions:NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
             authorization: {
                 params: {
-                  prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                    prompt: "consent",
                 }
             },
         }),    
     ],
+    callbacks: {
+        async signIn({ account, profile }) {
+          if (account.provider === "google") {
+            return profile.email_verified && profile.email.endsWith("@google.com")
+          }
+          return true // Do different verification for other providers that don't have `email_verified`
+        },
+    },
     secret: process.env.SECRET,
 };
 // T
